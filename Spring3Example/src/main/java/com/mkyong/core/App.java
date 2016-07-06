49686455e067e1939c1896a96edbd58215b0cb17
jianliu <jianliu99@gmail.com>
2016-07-06 10:52:08 -0400

    adding an alternative way.

--- Spring3Example/src/main/java/com/mkyong/core/App.java
+++ Spring3Example/src/main/java/com/mkyong/core/App.java
@@ -2,14 +2,16 @@ package com.mkyong.core;
  
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.mkyong.config.AppConfig;
 import com.mkyong.hello.HelloWorld;
  
 public class App {
 	public static void main(String[] args) {
 	    
-		//ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
-		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+		//ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 	    HelloWorld obj = (HelloWorld) context.getBean("helloBean");
 	    
 	    obj.printHelloWorld("Spring3 Java Config");

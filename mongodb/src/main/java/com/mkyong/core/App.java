a9dab5d51504e48d037290dee7e0a340b2bd25de
jliu1234 <jliu@jliu-2283.global-travel.on.ca>
2016-07-06 18:23:49 -0400

    test conn

--- mongodb/src/main/java/com/mkyong/core/App.java
+++ mongodb/src/main/java/com/mkyong/core/App.java
@@ -21,11 +21,11 @@ public class App {
 			/**** Connect to MongoDB ****/
 			// Since 2.10.0, uses MongoClient
 			MongoClient mongo = new MongoClient("localhost", 27017);
-
+			
 			/**** Get database ****/
 			// if database doesn't exists, MongoDB will create it for you
 			DB db = mongo.getDB("testdb");
-
+			System.out.println("DB conn good");
 			/**** Get collection / table from 'testdb' ****/
 			// if collection doesn't exists, MongoDB will create it for you
 			DBCollection table = db.getCollection("user");

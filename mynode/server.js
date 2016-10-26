cc8a09ba65befb32a8c63b31898ab545074f8940
jianliu99 <jianliu99@gmail.com>
2016-10-26 17:58:15 -0400

    my node.js

---
+++ mynode/server.js
@@ -0,0 +1,14 @@
+/**
+ * http://usejsdoc.org/
+ */
+var http = require("http");
+http.createServer(function (request, response) {
+	   // Send the HTTP header 
+	   // HTTP Status: 200 : OK
+	   // Content Type: text/plain
+	   response.writeHead(200, {'Content-Type': 'text/plain'});
+	   
+	   // Send the response body as "Hello World"
+	   response.end('Hello World\n');
+	}).listen(9999);
+
